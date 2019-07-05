8d6dd872da779fe46bfb0a93d902aeff54ec1451
ArturU043 <oudot.artur@hotmail.com>
2019-07-05 14:01:41 +0200

    Create plotLepPt.C

---
+++ plotLepPt.C
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+#include <cmath>
+#include <sstream>
+
+#include "TROOT.h"
+#include "TFile.h"
+#include "TTree.h"
+#include "TCut.h"
+#include "TChain.h"
+#include "TH1D.h"
+#include "TCanvas.h"
+#include "TGraphErrors.h"
+#include "string"
+#include "vector"
+
+
+void plotLepPt(){
+  // Load files
+
+  // access bdtt
+
+
+  // plot
+
+  //SAVE
+
+}
