1d44cbdcbe279f176aadb9109c1278d31ce7867e
timCabos <52538803+timCabos@users.noreply.github.com>
2019-07-07 14:34:18 +0200

    Update plotim.c

--- plotim.c
+++ plotim.c
@@ -9,11 +9,11 @@ TChain uno("bdttre");
  //uno.Add("/home/t3cms/dbastos/LSTORE/Stop4Body/nTuples16_v2017-10-19/T2DegStop_deltaM30.root");
 
 // lists
-newname[]={"new_XS", "new_Jet1Pt", "new_Met", "new_mt", "new_LepEta", "new_LepChg", "new_HT", "new_NbLoose", "new_Njet", "new_JetHBpt", "new_DrJetHBLep","new_JetHBCSV"};
-oldname[]= {"XS", "Jet1Pt", "Met", "mt", "LepEta", "LepChg", "HT", "NbLoose", "Njet", "JetHBpt", "DrJetHBLep","JetHBCSV"};
+string newname[12]={"new_XS", "new_Jet1Pt", "new_Met", "new_mt", "new_LepEta", "new_LepChg", "new_HT", "new_NbLoose", "new_Njet", "new_JetHBpt", "new_DrJetHBLep","new_JetHBCSV"};
+string oldname[12]= {"XS", "Jet1Pt", "Met", "mt", "LepEta", "LepChg", "HT", "NbLoose", "Njet", "JetHBpt", "DrJetHBLep","JetHBCSV"};
 
 //new graphs
-i=0
+i=0;
 
 while (i<12){
   //TH1D* newname[i] = new TH1D("newname[i]", "oldname[i]", 200,0,200);
