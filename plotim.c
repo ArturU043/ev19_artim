8b64a77ee03eff0c45b0d569dbe5acf1d56ab86b
timCabos <52538803+timCabos@users.noreply.github.com>
2019-07-07 17:20:30 +0200

    Update plotim.c

--- plotim.c
+++ plotim.c
@@ -1,4 +1,5 @@
 #include<stdio.h>
+#include<string.h>
 
 void plotim(){
 
@@ -9,21 +10,26 @@ TChain uno("bdttre");
  //uno.Add("/home/t3cms/dbastos/LSTORE/Stop4Body/nTuples16_v2017-10-19/T2DegStop_deltaM30.root");
 
 // lists
-string newname[12]={"new_XS", "new_Jet1Pt", "new_Met", "new_mt", "new_LepEta", "new_LepChg", "new_HT", "new_NbLoose", "new_Njet", "new_JetHBpt", "new_DrJetHBLep","new_JetHBCSV"};
-string oldname[12]= {"XS", "Jet1Pt", "Met", "mt", "LepEta", "LepChg", "HT", "NbLoose", "Njet", "JetHBpt", "DrJetHBLep","JetHBCSV"};
+char* newname[20]={"new_XS", "new_Jet1Pt", "new_Met", "new_mt", "new_LepEta", "new_LepChg", "new_HT", "new_NbLoose", "new_Njet", "new_JetHBpt", "new_DrJetHBLep","new_JetHBCSV"};
+char* oldname[20]= {"XS", "Jet1Pt", "Met", "mt", "LepEta", "LepChg", "HT", "NbLoose", "Njet", "JetHBpt", "DrJetHBLep","JetHBCSV"};
+
+printf("%s", newname[3]);
+
 
 //new graphs
-int i=0;
+/*int i=0;
 
-while (i<12){
+do
+
+while (i<12; i++){
   //TH1D* newname[i] = new TH1D("newname[i]", "oldname[i]", 200,0,200);
   //bdttree->Draw("oldname[i]>>newname[i]");
   string a = newname[i];
   string b = oldname [i];
 
   printf("%s \n", &a) ;
-  printf("%s \n", &b) ;
-  i=i+1;
+  printf("%s \n", &b) ;*/
+
 }
 
 
