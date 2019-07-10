05556a0f115e8ef8f5dcf9936dc41efc47fe99cc
timCabos <52538803+timCabos@users.noreply.github.com>
2019-07-10 16:21:03 +0200

    update local

--- StopNN/localConfig.py
+++ StopNN/localConfig.py
@@ -1,1 +1,11 @@
+#!/usr/bin/env python
 
+#path to .root datasets
+#loc = "/gstore/t3cms/store/user/cbeiraod/Stop4Body/storage/"
+#loc = "/lstore/cms/cbeiraod/Stop4Body/"
+#loc = "/lstore/cms/dbastos/Stop4Body/"
+loc = "/home/t3cms/dbastos/LSTORE/Stop4Body/"
+#loc = "/lstore/cms/dbastos/Stop4BodyChimera/"
+#loc = "/home/diogo/LIP/DATA/"
+#path to logbook
+lgbk = "/home/t3cms/ev19u045/LSTORE/ev19_artim/lgbk"
