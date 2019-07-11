9b1c39d0be43adc3c3aa153ac99fa29016254caf
ArturU043 <oudot.artur@hotmail.com>
2019-07-11 17:53:28 +0100

    plotter to commonF

--- StopNN/commonFunctions.py
+++ StopNN/commonFunctions.py
@@ -297,3 +297,21 @@ def arange(array, min, max):
     for i in range(min,max+1):
         array = array + [i]
     return array
+
+
+# Plots
+
+import os
+import matplotlib.pyplot as plt
+import sys
+import pickle
+
+
+def plotter(path,Ylabel,Title):
+    open_= open(path,'rb')
+    plot_= pickle.load(open_)
+    plt.plot(plot_)
+    plt.ylabel(Ylabel)
+    plt.xlabel("Epochs")
+    plt.legend()
+    plt.title(Title)
