fe494959774b373d73aeffadc6d849f08f4fd205
timCabos <52538803+timCabos@users.noreply.github.com>
2019-07-24 21:09:25 +0200

    wrong print

--- StopNN/FOM_Average.py
+++ StopNN/FOM_Average.py
@@ -32,8 +32,8 @@ if __name__ == "__main__":
 
         i = i + 1
 
-    print("list_Cut")
-    print("list_Evo")
+    print("%s".format(list_Cut))
+    print("%s".format(list_Evo))
     
     #Creating a new file where average will be stored:
 #    f = open(filepath+"Average_of_FOM_evo_data.txt","w+")
