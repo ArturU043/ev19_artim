d476cc244ed22a3fa6f77cd29bc4a395fed82cff
timCabos <52538803+timCabos@users.noreply.github.com>
2019-07-25 15:34:04 +0200

    envelope

--- StopNN/FOM_Average.py
+++ StopNN/FOM_Average.py
@@ -11,10 +11,12 @@ if __name__ == "__main__":
 
     parser = argparse.ArgumentParser(description="Chose models to plot fom")
     parser.add_argument('-i', '--version', type=str, required=True, help="Select the model to average and plot")
+    parser.add_argument('-env', '--env', action='store_true',  help="Used to draw an envelope in the average plot")
 
     args = parser.parse_args()
     paul = args.version
     list = paul.split()
+
 
     #Creating a list with filepaths:
     testpath = cfg.lgbk+"test/"
@@ -24,18 +26,21 @@ if __name__ == "__main__":
         path.append(testpath+"Model_Ver_"+str(list[i])+"/plots_Model_Ver_"+str(list[i])+"/")
         i = i + 1
 
-    #Importing data in lists used to calculate average:
+    #Importing data in lists used to compute average:
     i = 0
     list_Evo = []
     while (i < len(path)):
         list_Evo.append(np.loadtxt(path[i]+"FOM_evo_data.txt",delimiter="\n"))
         i = i + 1
 
+    #Initializing
     i = 0
     list_ave_Evo = []
     while (i < len(list_Evo[1])):
         list_ave_Evo.append(0.0)
         i = i + 1
+
+    #Computing average
     i = 0
     while (i < len(list_Evo[1])):
         a = 0
@@ -45,6 +50,33 @@ if __name__ == "__main__":
             a = a + 1
         list_ave_Evo[i] = ave_Evo/len(list)
         i = i + 1
+
+    #Envelope:
+    if args.env:
+        #Initializing envelope
+        list_max_env = []
+        list_min_env = []
+        i = 0
+        while i < len((list_Evo[1])):
+            list_max_env.append(0.0)
+            list_min_env.append(0.0)
+            i = i + 1
+        #taking envelope's values
+        i = 0
+        while (i < len(list_Evo[1])):
+            a = 0
+            b = 0
+            c = 100000000
+            while (a < len(list)):
+                if list_Evo[a][i] > b :
+                    b = list_Evo[a][i]
+                if list_Evo[a][i] < c :
+                    c = list_Evo[a][i]
+                a = a + 1
+            list_max_env[i] = b
+            list_min_env[i] = c
+            i = i + 1
+
 
     #Creating a new file where average will be stored:
     averagepath = testpath + "Model_Ver_" + paul[:2] + "_average/plots_Model_Ver_" + paul[:2] + "_average/"
@@ -57,3 +89,13 @@ if __name__ == "__main__":
     f.close()
 
     copyfile(testpath + "Model_Ver_10/plots_Model_Ver_10/FOM_cut_data.txt", averagepath + "FOM_cut_data.txt")
+
+    if args.env:
+
+        f = open(averagepath + "FOM_evo_data.txt","w+")
+        f.write("\n".join(map(str,list_max_env)))
+        f.close()
+
+        f = open(averagepath + "FOM_evo_data.txt","w+")
+        f.write("\n".join(map(str,list_min_env)))
+        f.close()
