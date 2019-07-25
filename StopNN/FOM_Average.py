347fb156270a5dc82d41681fb8615d4a8c7d7aaa
timCabos <52538803+timCabos@users.noreply.github.com>
2019-07-25 11:57:17 +0200

    tryin'

--- StopNN/FOM_Average.py
+++ StopNN/FOM_Average.py
@@ -18,11 +18,11 @@ if __name__ == "__main__":
     testpath = cfg.lgbk+"test/"
     i = 0
     path = []
-    while i<len(list) :
+    while (i < len(list)) :
         path.append(testpath+"Model_Ver_"+str(list[i])+"/plots_Model_Ver_"+str(list[i])+"/")
         i = i + 1
 
-    #Importing data used to calculate average:
+    #Importing data in lists used to calculate average:
     i = 0
     list_Cut = []
     list_Evo = []
@@ -32,8 +32,17 @@ if __name__ == "__main__":
 
         i = i + 1
 
-    print(list_Cut[0][0], list_Cut[1][0])
-    print(list_Evo[0][0], list_Evo[1][0])
+#    i = 0
+#    list_sum_Cut = []
+#    list_sum_Evo = []
+#    while (i < len(list_Cut)):
+#        list_sum_Cut = list_Cut[][]
+#        list_sum_Evo
+
+#        i = i + 1
+
+    print(list_Cut, list_Cut[1][0])
+    print(list_Evo, list_Evo[1][0])
 
     #Creating a new file where average will be stored:
 #    f = open(filepath+"Average_of_FOM_evo_data.txt","w+")
