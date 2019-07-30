da4349b0c4f431743302991a4c81e9a19ccac0c0
EV 19 Artur Oudot <ev19u043@cassini01.ncg.ingrid.pt>
2019-07-30 09:53:08 +0100

    summ

--- StopNN/FOM_Summary.py
+++ StopNN/FOM_Summary.py
@@ -11,7 +11,7 @@ if __name__ == "__main__":
 
     parser=argparse.ArgumentParser(description="Chose models to plot fom")
     parser.add_argument('-i', '--version', type=str, required=True, help="Select the models to plot together in same plot graphic with differents plots foms ")
-
+    parser.add_argument('-env', '--env', action='store_true',  help="Used to draw an envelope in the average plot")
     args=parser.parse_args()
     list=args.version
     list=list.split()
@@ -41,6 +41,34 @@ if __name__ == "__main__":
         print("Model_"+list[i]+" With max fom {}".format(max_FOM))
         max_FOM= round (max_FOM,4)
         plt.plot(fom_Cut, fom_Evo, label="Model_{}  Max Fom={}".format(list[i],max_FOM))
+
+
+
+        if args.env :
+            if os.path.exists(path[i]+"FOM_max_data.txt"):
+                fom_Evo_max = np.loadtxt(path[i]+"FOM_max_data.txt",delimiter="\n")
+
+                max_FOM = 0
+                for k in fom_Evo_max:
+                    if k>max_FOM:
+                        max_FOM=k
+
+                max_FOM= round (max_FOM,4)
+
+                plt.plot(fom_Cut, fom_Evo_max, label="Model_{} max envelope  Max Fom={}".format(list[i],max_FOM), linestyle='dashed')
+
+            if os.path.exists(path[i]+"FOM_min_data.txt"):
+                fom_Evo_min = np.loadtxt(path[i]+"FOM_min_data.txt",delimiter="\n")
+                max_FOM = 0
+                for k in fom_Evo_min:
+                    if k>max_FOM:
+                        max_FOM=k
+
+                max_FOM= round (max_FOM,4)
+
+                plt.plot(fom_Cut, fom_Evo_min, label="Model_{} min envelope  Max Fom={}".format(list[i],max_FOM), linestyle='dashed')
+
+
         i=i+1
 
     plt.title("FOM Comparing")
@@ -48,3 +76,4 @@ if __name__ == "__main__":
     plt.xlabel("NN output")
     plt.legend()
     plt.show()
+
